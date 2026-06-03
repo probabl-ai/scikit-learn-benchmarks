b157b9a5c3718560db9b523fef82b8c159465622
Arthur Lacote <arthur.lcte@gmail.com>
2026-06-03 11:27:25 +0200

    fix test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 pixi run -e sklearn python -m sklbench --config configs/sklearn.json
-pixi run -e nightly python -m sklbench --config configs/array-api-intel.json
+pixi run -e sk-intel python -m sklbench --config configs/array-api-intel.json
 pixi run -e intel python -m sklbench --config configs/sklearnex.json
