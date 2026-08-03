058a3ff1e203972c1edf929de9d3d1621c6c8376
Arthur Lacote <arthur.lcte@gmail.com>
2026-08-03 10:32:03 +0200

    update tests

--- tests/test_config_loading.py
+++ tests/test_config_loading.py
@@ -19,6 +19,7 @@ ARRAY_API_ENVS = [*GENERAL_ENVS, "skl-cpu", "skl-intel", "skl-nvidia"]
 ENV_SENSITIVE_CONFIGS = {
     Path("configs/all_models_test.py"): ARRAY_API_ENVS,
     Path("configs/all_models_fast.py"): ARRAY_API_ENVS,
+    Path("configs/all_models.py"): ARRAY_API_ENVS,
 }
 
 
