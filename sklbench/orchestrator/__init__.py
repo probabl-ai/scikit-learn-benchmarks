fbc232aa274b0ca75a36eb3143cec6bafe07ae94
Arthur Lacote <arthur.lcte@gmail.com>
2026-08-03 10:38:34 +0200

    load only flag

--- sklbench/orchestrator/__init__.py
+++ sklbench/orchestrator/__init__.py
@@ -1,5 +1,6 @@
 from .arguments import get_orchestrator_parser
 from .implementation import (
+    load_datasets_only,
     orchestrate_benchmarks,
     save_benchmark_record,
     save_environment_sidecars,
@@ -7,6 +8,7 @@ from .implementation import (
 
 __all__ = [
     "get_orchestrator_parser",
+    "load_datasets_only",
     "orchestrate_benchmarks",
     "save_benchmark_record",
     "save_environment_sidecars",
