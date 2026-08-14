282d2d5364a831068a072c6b37f4c68333366d96
Arthur Lacote <arthur.lcte@gmail.com>
2026-08-14 14:48:40 +0200

    HGB laptop config

--- configs/hgb_scaling_laptop.py
+++ configs/hgb_scaling_laptop.py
@@ -6,11 +6,12 @@ def generate_cases() -> list[dict]:
         {
             **case,
             "bench": {
-                **case["bench"],
+                "n_runs": 5,
                 "env": {
-                    **case["bench"]["env"],
                     "GOMP_SPINCOUNT": "300000",
+                    "OMP_NUM_THREADS": case["bench"]["env"]["OMP_NUM_THREADS"],
                 },
+                "py_spy_profiling": False,
             },
         }
         for case in hgb_scaling.generate_cases()
