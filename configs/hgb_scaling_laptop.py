875a984f019eea59aa9b315b856c804118f7796f
Arthur Lacote <arthur.lcte@gmail.com>
2026-08-18 17:24:21 +0200

    tmp: disable GOMP_SPINCOUNT

--- configs/hgb_scaling_laptop.py
+++ configs/hgb_scaling_laptop.py
@@ -8,7 +8,7 @@ def generate_cases() -> list[dict]:
             "bench": {
                 "n_runs": 5,
                 "env": {
-                    "GOMP_SPINCOUNT": "300000",
+                    # "GOMP_SPINCOUNT": "300000",
                     "OMP_NUM_THREADS": case["bench"]["env"]["OMP_NUM_THREADS"],
                 },
                 "py_spy_profiling": False,
