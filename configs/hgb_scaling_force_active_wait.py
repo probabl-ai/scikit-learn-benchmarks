2c39531b9a286e44373e10c4c691e1ebef17564a
Arthur Lacote <arthur.lcte@gmail.com>
2026-08-19 16:43:35 +0200

    fix

---
+++ configs/hgb_scaling_force_active_wait.py
@@ -0,0 +1,19 @@
+import hgb_scaling
+
+
+def generate_cases() -> list[dict]:
+    return [
+        {
+            **case,
+            "bench": {
+                "n_runs": 5,
+                "env": {
+                    "GOMP_SPINCOUNT": "300000",
+                    "KMP_BLOCKTIME": "200ms",
+                    "OMP_NUM_THREADS": case["bench"]["env"]["OMP_NUM_THREADS"],
+                },
+                "py_spy_profiling": False,
+            },
+        }
+        for case in hgb_scaling.generate_cases()
+    ]
